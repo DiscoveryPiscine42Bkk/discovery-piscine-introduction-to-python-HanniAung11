40dbe1d0da2b48916d10b7445456fce791be301e
Hanni <ephoney1141@gmail.com>
2025-06-23 23:47:33 -0700

    Add files via upload

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) == 3:
+    try:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+        
+        numbers = list(range(start, end + 1))
+        print(numbers)
+    except ValueError:
+        print("none")
+else:
+    print("none")
